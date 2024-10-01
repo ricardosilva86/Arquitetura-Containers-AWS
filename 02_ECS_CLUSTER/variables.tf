24a69d1cf9f7fd99eab262f09ca64aa7b5c51864
Ricardo Silva <ricardolopes18@yahoo.com.br>
2024-10-02 00:00:09 +0200

    Aula 02: ECS Cluster

---
+++ 02_ECS_CLUSTER/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {}
+
+variable "region" {}
+
+variable "vpc_id" {}
+variable "private_subnet_1a" {}
+variable "private_subnet_1b" {}
+variable "private_subnet_1c" {}
+variable "public_subnet_1a" {}
+variable "public_subnet_1b" {}
+variable "public_subnet_1c" {}
+variable "database_subnet_1a" {}
+variable "database_subnet_1b" {}
+variable "database_subnet_1c" {}
